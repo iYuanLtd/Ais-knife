ddc80f05f3c1987e3f3cde097e78b8899d0d9d75
iYuan <yuan@iyuan.ltd>
2022-08-19 12:34:45 +0800

    🎉 V1.0.0

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<script setup>
+// import Knife3DModel from "./knife3DModel.vue";
+
+</script>
+
+<template>
+  <div class="min-h-screen bg-gradient-to-tr from-blue-500 to-rose-600 backdrop-blur-xl">
+    <div class="h-[400px]">
+      <!-- <Knife3DModel /> -->
+      <div class="flex justify-center items-center">
+        <img src="/1.png" class="w-[300px] -mt-40 animate__animated animate__pulse animate__slower animate__infinite">
+      </div>
+    </div>
+
+    <div class="py-4 mx-5 flex justify-center items-center text-slate-100 flex-col">
+      <div class="font-extrabold text-5xl text-center leading-snug mb-8">
+        <p>Ais</p>
+        <p>一千块开出</p>
+        <p>价值4w的蓝宝石锯齿爪刀</p>
+        <p>磨损0.0037503</p>
+        <p>世界级磨损</p>
+        <p>目前buff上估价为4万</p>
+        <p>望周知</p>
+      </div>
+
+      <div class="flex justify-center items-center flex-col">
+        <img class="mb-8 rounded-2xl" src="/qrcode.jpg">
+        <img class="mb-8 rounded-2xl" src="/gravestone.jpg">
+        <img class="mb-8 rounded-2xl" src="/shot.jpg">
+      </div>
+
+      <div class="grid grid-cols-1 md:grid-cols-2 max-w-3xl gap-4">
+        <img class="rounded-2xl" src="/emotion/1.jpg">
+        <img class="rounded-2xl h-[165px]" src="/emotion/4.jpg">
+        <img class="rounded-2xl h-[156px]" src="/emotion/2.jpg">
+        <img class="rounded-2xl" src="/emotion/3.png">
+        <img class="rounded-2xl" src="/emotion/5.jpg">
+        <img class="rounded-2xl" src="/emotion/7.gif">
+        <img class="rounded-2xl" src="/emotion/6.png">
+        <img class="rounded-2xl" src="/emotion/8.png">
+      </div>
+
+      <div class="mt-20 font-extrabold text-slate-300">
+        © iYuan
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/knife3DModel.vue
@@ -0,0 +1,8 @@
+<template>
+  <model-fbx
+    src="/knife.fbx"
+  />
+</template>
+<script setup>
+import { ModelFbx } from 'vue-3d-model';
+</script>
